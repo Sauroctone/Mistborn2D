23c624b9bee1223db6eaad1d59a6d0ae607d1524
Sauroctone <neo.laq.33@gmail.com>
2017-09-20 23:22:38 +0200

    Debut

---
+++ Assets/Scripts/IronSteelController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronSteelController : MonoBehaviour {
+
+	public LineRenderer line;
+	private bool isClicking;
+	private RaycastHit2D hit;
+	private Rigidbody2D rb;
+
+	public float ppForce; //push pull force
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		if (!isClicking) 
+		{
+			if (Input.GetMouseButtonDown (0) || Input.GetMouseButtonDown(1)) 
+			{
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				hit = Physics2D.Raycast (ray.origin, ray.direction);
+
+				if (hit.collider != null && hit.collider.tag == "Metal") 
+				{
+					isClicking = true;
+				}
+			}
+		}
+
+		if (isClicking)
+		{
+			line.gameObject.SetActive (true);
+			line.SetPosition (0, transform.position);
+			line.SetPosition (1, hit.transform.position);
+
+			if (Input.GetMouseButton (0))
+				rb.AddForce ((transform.position - hit.transform.position).normalized * ppForce, ForceMode2D.Impulse);
+
+			if (Input.GetMouseButton (1))
+				rb.AddForce ((hit.transform.position - transform.position).normalized.normalized * ppForce, ForceMode2D.Impulse);
+		}
+
+		if (Input.GetMouseButtonUp(0) || Input.GetMouseButtonUp(1))
+		{
+			isClicking = false;
+			line.gameObject.SetActive (false);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private float lastXInput;
+	public float speed = 5f;
+	public float jumpForce = 3f;
+	public float maxVelocity;
+	private float movLerp;
+	public float airControl;
+
+	private Rigidbody2D rb;
+	private bool isGrounded = false;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate()
+	{
+		float xinput = Input.GetAxisRaw ("Horizontal");	
+
+		if (isGrounded || xinput != 0)
+			rb.velocity = Vector2.Lerp (rb.velocity, new Vector2 (xinput * speed, rb.velocity.y), movLerp);
+
+		if (isGrounded) 
+		{
+			if (Input.GetButtonDown ("Jump")) 
+			{
+				rb.AddForce (transform.up * jumpForce, ForceMode2D.Impulse);
+			}
+
+			movLerp = 1f;
+		}
+
+		else
+		{
+			movLerp = airControl;
+		}
+			
+		rb.velocity = Vector2.ClampMagnitude (rb.velocity, maxVelocity);
+	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.tag == "Floor") 
+		{
+			isGrounded = true;
+		}
+	}		// isGrounded = true;
+
+	void OnTriggerExit2D (Collider2D col)
+	{
+		if (col.tag == "Floor") 
+		{
+			isGrounded = false;
+		}
+	} 	//isGrounded = false;
+}
